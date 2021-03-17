8d32461340c277a59b17d221ec88ede69fa77bab
Chris Adkin <chris@exadat.co.uk>
2021-03-17 16:42:52 +0000

    Terraform block added to main.tf to specified required provider versions

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,3 +1,39 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "1.4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.48.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      # FIXME: Pinned version due to Helm provider bug in 2.0.0 & 2.0.1 regarding existing Terraform state
+      # "Warning: Release does not exist"
+      # See: https://github.com/hashicorp/terraform-provider-helm/issues/662
+      version = "= 1.3.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "= 2.0.2"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
 provider "kubernetes" {
   config_path    = "~/.kube/config"
 }
@@ -18,4 +54,8 @@ module "px_store" {
 
 module "px_backup" {
   source = "./modules/px_backup"
+}
+
+module "metallb" {
+  source = "./modules/metallb"
 }
